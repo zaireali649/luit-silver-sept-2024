90ab5350bf95579e436d36b60ab5f02ae96db392
Zaire Ali <zaireali649@gmail.com>
2024-09-12 19:29:41 -0400

    Create testing_imports.py

---
+++ testing_imports.py
@@ -0,0 +1,7 @@
+import random  # Import the random module to generate random numbers
+
+# Generate a random integer between 0 and 10 (inclusive) and assign it to 'number'
+number = random.randint(0, 10)
+
+# Print the randomly generated number
+print(number)
